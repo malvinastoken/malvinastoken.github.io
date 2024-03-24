9accf2df31216cd186328b4238caf6c440dc2c09
unknown <Mateo>
2024-03-24 15:52:29 -0300

    Init

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="ES">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img//apple-icon.png">
+    <link rel="icon" type="image/png" href="./assets/img//favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>
+        Malvinas Token - $MLV
+    </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+        name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./assets/css/paper-kit.css?v=2.3.1" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="./assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+
+<body class="index-page ">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg fixed-top navbar-default shadow-none py-3" color-on-scroll="500">
+        <div class="container">
+            <div class="navbar-translate">
+                <a class="navbar-brand" href="https://demos.creative-tim.com/paper-kit-2-pro/index.html" rel="tooltip"
+                    title="Paper Kit 2 PRO" data-placement="bottom" target="_blank">
+                    Malvinas Token
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+                    aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar bar1"></span>
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" data-nav-image="./assets/img/blurred-image-1.jpg" data-color="orange">
+                <ul class="navbar-nav ml-auto">
+
+
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container  py-5 my-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-md-12 col-sm-12 col-lg-5    ">
+            </div>
+        </div>
+        <div class=" col-sm-12 col-md-12 col-lg-7">
+            <h1 class="display-3 fw-bold lh-1 text-left text-dark  ">Malvinas Token </h1>
+            <p class="lead my-3  text-dark">Tan nuestros como las islas.</p>
+            <div class="row">
+                <div class=" col-sm-12 col-md-4 col-xl-4">
+                    <a class="btn btn-neutral btn-round text-dark  shadow-sm btn-block ">WHITEPAPER</a>
+                </div>
+                <div class=" col-sm-12 col-md-4 col-xl-4">
+                    <a class="btn btn-default btn-round  text-white shadow-sm btn-block mx-2 disabled">COMPRAR $MLV</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+    <h2 class="text-center">Nuestra Misión</h2>
+    <h2 class="text-center">¿Por que Malvinas Token?</h2>
+    <h2 class="text-center">Tokenomics</h2>
+
+
+    <!--   Core JS Files   -->
+    <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
+    <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
+    <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script src="./assets/js/paper-kit.js?v=2.3.1" type="text/javascript"></script>
+</body>
+
+</html>
